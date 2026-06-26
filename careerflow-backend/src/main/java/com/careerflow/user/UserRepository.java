ce8692774b6fb511c0e9fd9502d9a4573b2bd8c5
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-06-26 10:41:58 +0400

    Updated UserRepository

--- careerflow-backend/src/main/java/com/careerflow/user/UserRepository.java
+++ careerflow-backend/src/main/java/com/careerflow/user/UserRepository.java
@@ -9,5 +9,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByUsername(String username);
+
     boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
