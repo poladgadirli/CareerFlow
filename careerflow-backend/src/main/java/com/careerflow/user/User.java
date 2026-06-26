7e7bee49a7c153c7c2d98bd26d55c64c664839fa
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-06-26 10:30:30 +0400

    Created User entity

---
+++ careerflow-backend/src/main/java/com/careerflow/user/User.java
@@ -0,0 +1,26 @@
+package com.careerflow.user;
+
+import com.careerflow.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+}
